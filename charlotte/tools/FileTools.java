2a24b12f5e3f8a2b7250c9eccfb0e720b3bd0b0d
stackprobe <stackprobes@gmail.com>
2018-11-05 23:51:38 +0900

    daily

--- charlotte/tools/FileTools.java
+++ charlotte/tools/FileTools.java
@@ -1,11 +1,181 @@
 package charlotte.tools;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.List;
 
 public class FileTools {
+	public static void delete(String path) throws Exception {
+		File f = new File(path);
+
+		if(f.exists()) {
+			for(int c = 1; ; c++) {
+				try {
+					deleteFileOrDirectory(path);
+
+					if(f.exists() == false) {
+						break;
+					}
+				}
+				catch(Throwable e) {
+					e.printStackTrace(System.out);
+				}
+				if(10 < c) {
+					throw new RTError("ファイルの削除に失敗しました。" + path);
+				}
+				System.out.println("ファイルの削除をリトライします。" + path);
+
+				Thread.sleep(c * 100);
+			}
+		}
+	}
+
+	private static void deleteFileOrDirectory(String path) throws Exception {
+		File f = new File(path);
+
+		if(f.isDirectory()) {
+			for(String childLocalName : f.list()) {
+				deleteFileOrDirectory(combine(path, childLocalName));
+			}
+		}
+		if(f.delete() == false) {
+			throw new RTError("ファイル又はディレクトリの削除に失敗しました。" + path);
+		}
+	}
+
+	private static String combine(String path1, String path2) {
+		String path = path1 + "/" + path2;
+
+		boolean windowsNetworkPath = path.startsWith("\\\\");
+
+		if(windowsNetworkPath) {
+			path = path.substring(1);
+		}
+
+		path = path.replace("\\", "/");
+		path = StringTools.replaceLoop(path, "//", "/");
+
+		if(windowsNetworkPath) {
+			path = path.replace("/", "\\");
+			path = "\\" + path;
+		}
+		return path;
+	}
+
+	public static void createDir(String dir) {
+		File d = new File(dir);
+
+		if(d.isDirectory() == false && new File(dir).mkdirs() == false) {
+			throw new RTError("ディレクトリの作成に失敗しました。" + dir);
+		}
+	}
+
+	public static void cleanupDir(String dir) throws Exception {
+		for(String childLocalName : new File(dir).list()) {
+			delete(combine(dir, childLocalName));
+		}
+	}
+
+	public static void copyDir(String rDir, String wDir) throws Exception {
+		createDir(wDir);
+
+		for(String childLocalName : new File(rDir).list()) {
+			String rPath = combine(rDir, childLocalName);
+			String wPath = combine(wDir, childLocalName);
+
+			if(new File(rPath).isDirectory()) {
+				copyDir(rPath, wPath);
+			}
+			else {
+				copyFile(rPath, wPath);
+			}
+		}
+	}
+
+	public static void copyFile(String rFile, String wFile) throws Exception {
+		byte[] buff = new byte[4 * 1024 * 1024];
+
+		try(
+				FileInputStream reader = new FileInputStream(rFile);
+				FileOutputStream writer = new FileOutputStream(wFile);
+				) {
+			for(; ; ) {
+				int readSize = reader.read(buff);
+
+				if(readSize <= 0) {
+					break;
+				}
+				writer.write(buff, 0, readSize);
+			}
+		}
+	}
+
+	public static void moveDir(String rDir, String wDir) {
+		createDir(wDir);
+
+		for(String childLocalName : new File(rDir).list()) {
+			String rPath = combine(rDir, childLocalName);
+			String wPath = combine(wDir, childLocalName);
+
+			if(new File(rPath).isDirectory()) {
+				moveDir(rPath, wPath);
+			}
+			else {
+				moveFile(rPath, wPath);
+			}
+		}
+	}
+
+	public static void moveFile(String rFile, String wFile) {
+		if(new File(rFile).renameTo(new File(wFile)) == false) {
+			throw new RTError("ファイル又はディレクトリの移動に失敗しました。" + rFile + " -> " + wFile);
+		}
+	}
+
+	public static String changeRoot(String path, String oldRoot, String rootNew) {
+		oldRoot = putSlash(oldRoot);
+		rootNew = putSlash(rootNew);
+
+		if(StringTools.startsWithIgnoreCase(path, oldRoot) == false) {
+			throw new RTError("パスの配下ではありません。" + oldRoot + " -> " + path);
+		}
+		return rootNew + path.substring(oldRoot.length());
+	}
+
+	public static String putSlash(String path) {
+		if(path.endsWith("/") == false || path.endsWith("\\") == false) {
+			path += "/";
+		}
+		return path;
+	}
+
+	public static String makeFullPath(String path) throws Exception {
+		return new File(path).getCanonicalPath();
+	}
+
+	public static int compBinFile(String file1, String file2) throws Exception {
+		try(
+				FileInputStream nb_reader1 = new FileInputStream(file1);
+				FileInputStream nb_reader2 = new FileInputStream(file2);
+				BufferedInputStream reader1 = new BufferedInputStream(nb_reader1);
+				BufferedInputStream reader2 = new BufferedInputStream(nb_reader2);
+				) {
+			for(; ; ) {
+				int chr1 = reader1.read();
+				int chr2 = reader2.read();
+
+				if(chr1 != chr2) {
+					return chr1 - chr2;
+				}
+				if(chr1 == -1) {
+					return 0;
+				}
+			}
+		}
+	}
+
 	public static byte[] readAllBytes(File f) throws Exception {
 		long lFileSize = f.length();
 
--- tests/charlotte/tools/SpicaToolkitTest.java
+++ tests/charlotte/tools/SpicaToolkitTest.java
@@ -17,28 +17,28 @@ public class SpicaToolkitTest {
 	}
 
 	private static void test01() throws Exception {
-		Process p = SpicaToolkit.exec("/NAMED-EVENT AAA TTT " + ExtraTools.PID);
+		Process p = SpicaToolkit.exec("/NAMED-EVENT AAA EEE TTT " + ExtraTools.PID);
 
-		Thread.sleep(100);
+		Thread.sleep(100); // XXX
 		if(p.isAlive() == false) throw null;
 
 		Process q = SpicaToolkit.exec("/NAMED-EVENT-WAIT AAA -1 " + ExtraTools.PID);
 
-		Thread.sleep(100);
+		Thread.sleep(100); // XXX
 		if(p.isAlive() == false) throw null;
 		if(q.isAlive() == false) throw null;
 
 		Process r = SpicaToolkit.exec("/NAMED-EVENT-SET AAA");
 		r.waitFor();
 
-		Thread.sleep(100);
+		Thread.sleep(100); // XXX
 		if(p.isAlive() == false) throw null;
 		if(q.isAlive()) throw null;
 		if(r.isAlive()) throw null;
 
 		Process s = SpicaToolkit.exec("/NAMED-EVENT-SET TTT");
 
-		Thread.sleep(100);
+		Thread.sleep(100); // XXX
 		if(p.isAlive()) throw null;
 		if(s.isAlive()) throw null;
 	}
