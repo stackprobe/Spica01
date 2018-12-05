2c1135592b02932e49161d6f8b7cb8cd82bd5489
stackprobe <stackprobes@gmail.com>
2018-12-06 01:10:44 +0900

    daily

---
+++ charlotte/tools/ObjectTree.java
@@ -0,0 +1,122 @@
+package charlotte.tools;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ObjectTree implements Iterable<ObjectTree> {
+	private Object _root;
+
+	public ObjectTree(Object root) {
+		_root = root;
+	}
+
+	public ObjectTree get(int index) {
+		return new ObjectTree(((ObjectList)_root).get(index));
+	}
+
+	public ObjectTree get(String path) {
+		return get(StringTools.tokenize(path, "/"));
+	}
+
+	public ObjectTree get(Iterable<String> pTkns) {
+		Object node = _root;
+
+		for(String pTkn : pTkns) {
+			if(node instanceof ObjectList) {
+				node = ((ObjectList)node).get(Integer.parseInt(pTkn));
+			}
+			else if(node instanceof ObjectMap) {
+				node = ((ObjectMap)node).get(pTkn);
+			}
+			else {
+				throw new RTError("pTkn: " + pTkn);
+			}
+		}
+		return new ObjectTree(node);
+	}
+
+	public int size() {
+		if(_root instanceof ObjectList) {
+			return ((ObjectList)_root).size();
+		}
+		if(_root instanceof ObjectMap) {
+			return ((ObjectMap)_root).size();
+		}
+		throw new RTError("_root: " + _root);
+	}
+
+	public List<String> keys() {
+		if(_root instanceof ObjectList) {
+			return ListTools.select(IntTools.asList(IntTools.sequence(((ObjectList)_root).size())), index -> index.toString());
+		}
+		if(_root instanceof ObjectMap) {
+			return ((ObjectMap)_root).keys();
+		}
+		throw new RTError("_root: " + _root);
+	}
+
+	public String getString() {
+		return _root.toString();
+	}
+
+	@Override
+	public String toString() {
+		System.out.println("[DEBUG] _root: " + _root); // test
+
+		if(_root == null) {
+			return "_root == null";
+		}
+		return JsonTools.encode(_root);
+	}
+
+	public IArray<ObjectTree> toArray() {
+		if(_root instanceof ObjectList) {
+			return new IArray<ObjectTree>() {
+				@Override
+				public int length() {
+					return ((ObjectList)_root).size();
+				}
+
+				@Override
+				public ObjectTree get(int index) {
+					return new ObjectTree(((ObjectList)_root).get(index));
+				}
+
+				@Override
+				public void set(int index, ObjectTree element) {
+					throw new RTError("forbidden");
+				}
+			};
+		}
+
+		if(_root instanceof ObjectMap) {
+			List<String> keys = ((ObjectMap)_root).keys();
+
+			return new IArray<ObjectTree>() {
+				@Override
+				public int length() {
+					return keys.size();
+				}
+
+				@Override
+				public ObjectTree get(int index) {
+					String key = keys.get(index);
+
+					return new ObjectTree(ObjectList.create(new Object[] { key, ((ObjectMap)_root).get(key) }));
+				}
+
+				@Override
+				public void set(int index, ObjectTree element) {
+					throw new RTError("forbidden");
+				}
+			};
+		}
+
+		throw new RTError("_root: " + _root);
+	}
+
+	@Override
+	public Iterator<ObjectTree> iterator() {
+		return IArrays.asList(toArray()).iterator();
+	}
+}
---
+++ tests/charlotte/tools/ObjectTreeTest.java
@@ -0,0 +1,34 @@
+package tests.charlotte.tools;
+
+import charlotte.tools.JsonTools;
+import charlotte.tools.ObjectTree;
+
+public class ObjectTreeTest {
+	public static void main(String[] args) {
+		try {
+			test01();
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
+
+	private static void test01() {
+		{
+			ObjectTree ot = new ObjectTree(JsonTools.decode("{ 1: { 2: { 3: 777 }}}"));
+
+			System.out.println("777 == " + ot.get("1/2/3").getString());
+
+			System.out.println("ot: " + ot);
+		}
+
+		{
+			ObjectTree ot = new ObjectTree(JsonTools.decode("[ 1, [ 1, 2, [ 1, 2, 3, 777 ]]]"));
+
+			System.out.println("777 == " + ot.get("1/2/3").getString());
+
+			System.out.println("ot: " + ot);
+		}
+	}
+}
