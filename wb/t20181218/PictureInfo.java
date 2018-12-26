c97da58d77c91f342e4d927becbd939edd3c7660
stackprobe <stackprobes@gmail.com>
2018-12-27 01:19:53 +0900

    daily

--- wb/t20181218/PictureInfo.java
+++ wb/t20181218/PictureInfo.java
@@ -1,9 +1,11 @@
 package wb.t20181218;
 
+import charlotte.tools.RTError;
+
 public class PictureInfo {
 	private String _file;
 
-	public PictureInfo(String file) {
+	public PictureInfo(String file) throws Exception {
 		_file = file;
 	}
 
@@ -20,6 +22,10 @@ public class PictureInfo {
 	}
 
 	public double difference(PictureInfo other) {
+		return RTError.get(() -> difference_e(other));
+	}
+
+	public double difference_e(PictureInfo other) throws Exception {
 		throw null; // TODO
 	}
 }
