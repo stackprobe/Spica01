e71107bf72263ab57a668e4af9ddc2477f0c3706
stackprobe <stackprobes@gmail.com>
2019-07-19 08:04:09 +0900

    Berry

---
+++ charlotte/tools/VisitorCounter.java
@@ -0,0 +1,34 @@
+package charlotte.tools;
+
+public class VisitorCounter {
+	private int _count;
+
+	public VisitorCounter() {
+		this(0);
+	}
+
+	public VisitorCounter(int count) {
+		_count = count;
+	}
+
+	public boolean hasVisitor() {
+		return hasVisitor(1);
+	}
+
+	public boolean hasVisitor(int count) {
+		return count <= _count;
+	}
+
+	public void enter() {
+		_count++;
+	}
+
+	public void leave() {
+		_count--;
+	}
+
+	public AutoCloseable section() {
+		enter();
+		return () -> leave();
+	}
+}
