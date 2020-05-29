489c7b4ea36394b356067293a87dc60d9661bd1f
stackprobe <stackprobes@gmail.com>
2020-05-29 17:57:44 +0900

    Aqua

--- wb/t20200528/Test0001.java
+++ wb/t20200528/Test0001.java
@@ -3,6 +3,7 @@ package wb.t20200528;
 import java.util.List;
 
 import charlotte.tools.IntTools;
+import charlotte.tools.ListTools;
 import charlotte.tools.SecurityTools;
 
 public class Test0001 {
@@ -40,6 +41,8 @@ public class Test0001 {
 	private static void test01_a(int valueNum) {
 		for(int c = 0; c < 100; c++) {
 			test01_b(valueNum);
+			test01_c(valueNum);
+			test01_d(valueNum);
 		}
 	}
 
@@ -56,9 +59,81 @@ public class Test0001 {
 		}
 
 		for(int i = 0; i < valueNum; i++) {
+			//System.out.println(values.get(i)); // test
 			if(dest[i] != i) {
 				throw null; // souteigai !!!
 			}
 		}
+		//System.out.println(""); // test
+	}
+
+	private static void test01_c(int valueNum) {
+		List<Integer> values = IntTools.asList(IntTools.sequence(valueNum));
+		SecurityTools.cRandom.shuffle(values);
+
+		List<Integer> indexes = IntTools.asList(IntTools.sequence(valueNum));
+		indexes.sort((a, b) -> values.get(a) - values.get(b));
+
+		for(int i = 0; i < valueNum; i++) {
+			if(indexes.get(i) != -1) {
+				int xi = i;
+
+				for(; ; ) {
+					int yi = indexes.get(xi);
+
+					indexes.set(xi, -1);
+
+					if(yi == i) {
+						break;
+					}
+					ListTools.swap(values, xi, yi);
+					xi = yi;
+				}
+			}
+		}
+
+		for(int i = 0; i < valueNum; i++) {
+			//System.out.println(values.get(i)); // test
+			if(values.get(i) != i) {
+				throw null; // souteigai !!!
+			}
+		}
+		//System.out.println(""); // test
+	}
+
+	private static void test01_d(int valueNum) {
+		List<Integer> values = IntTools.asList(IntTools.sequence(valueNum));
+		SecurityTools.cRandom.shuffle(values);
+
+		List<Integer> indexes = IntTools.asList(IntTools.sequence(valueNum));
+		indexes.sort((a, b) -> values.get(a) - values.get(b));
+
+		for(int i = 0; i < valueNum; i++) {
+			if(indexes.get(i) != -1) {
+				int escVal = values.get(i);
+				int xi = i;
+
+				for(; ; ) {
+					int yi = indexes.get(xi);
+
+					indexes.set(xi, -1);
+
+					if(yi == i) {
+						break;
+					}
+					values.set(xi, values.get(yi));
+					xi = yi;
+				}
+				values.set(xi, escVal);
+			}
+		}
+
+		for(int i = 0; i < valueNum; i++) {
+			//System.out.println(values.get(i)); // test
+			if(values.get(i) != i) {
+				throw null; // souteigai !!!
+			}
+		}
+		//System.out.println(""); // test
 	}
 }
