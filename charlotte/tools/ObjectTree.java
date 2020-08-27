65f7c2ab113668964294845629ab63b086ab9995
stackprobe <stackprobes@gmail.com>
2020-08-27 18:46:34 +0900

    Prometheus

--- charlotte/tools/ObjectTree.java
+++ charlotte/tools/ObjectTree.java
@@ -5,10 +5,21 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 構造化オブジェクトをラップするクラス<br> // orig: * \u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3092\u30e9\u30c3\u30d7\u3059\u308b\u30af\u30e9\u30b9<br>
+ * 構造化オブジェクトは、以下のいずれかであるものを指す。<br> // orig: * \u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u306f\u3001\u4ee5\u4e0b\u306e\u3044\u305a\u308c\u304b\u3067\u3042\u308b\u3082\u306e\u3092\u6307\u3059\u3002<br>
+ * 1. 全ての要素が構造化オブジェクトである ObjectList<br> // orig: * 1. \u5168\u3066\u306e\u8981\u7d20\u304c\u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3067\u3042\u308b ObjectList<br>
+ * 2. 全ての要素の値が構造化オブジェクトである ObjectMap<br> // orig: * 2. \u5168\u3066\u306e\u8981\u7d20\u306e\u5024\u304c\u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3067\u3042\u308b ObjectMap<br>
+ * 3. object<br>
+ * 4. null
+ *
+ */
 public class ObjectTree implements Iterable<ObjectTree> {
 	/**
-	 * ただの構造化オブジェクトを JsonTools.Encode() に渡せるような構造化オブジェクトに変換する。 // orig: * \u305f\u3060\u306e\u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3092 JsonTools.Encode() \u306b\u6e21\u305b\u308b\u3088\u3046\u306a\u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u306b\u5909\u63db\u3059\u308b\u3002
-	 * @param root 構造化オブジェクト // orig: * @param root \u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8
+	 * 色々なオブジェクトを構造化オブジェクトに変換する。<br> // orig: * \u8272\u3005\u306a\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3092\u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u306b\u5909\u63db\u3059\u308b\u3002<br>
+	 * 本メソッドの用途は決まっていない。呼び出し側は処理が変更されることを想定すること。<br> // orig: * \u672c\u30e1\u30bd\u30c3\u30c9\u306e\u7528\u9014\u306f\u6c7a\u307e\u3063\u3066\u3044\u306a\u3044\u3002\u547c\u3073\u51fa\u3057\u5074\u306f\u51e6\u7406\u304c\u5909\u66f4\u3055\u308c\u308b\u3053\u3068\u3092\u60f3\u5b9a\u3059\u308b\u3053\u3068\u3002<br>
+	 * 現在のところ、自力で作ったオブジェクトを JsonTools.Encode() に渡せるような構造化オブジェクトに変換することを用途としている。 // orig: * \u73fe\u5728\u306e\u3068\u3053\u308d\u3001\u81ea\u529b\u3067\u4f5c\u3063\u305f\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3092 JsonTools.Encode() \u306b\u6e21\u305b\u308b\u3088\u3046\u306a\u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u306b\u5909\u63db\u3059\u308b\u3053\u3068\u3092\u7528\u9014\u3068\u3057\u3066\u3044\u308b\u3002
+	 * @param root 色々なオブジェクト // orig: * @param root \u8272\u3005\u306a\u30aa\u30d6\u30b8\u30a7\u30af\u30c8
 	 * @return 構造化オブジェクト // orig: * @return \u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8
 	 */
 	public static Object conv(Object root) {
@@ -56,9 +67,9 @@ public class ObjectTree implements Iterable<ObjectTree> {
 	}
 
 	/**
-	 * ただの構造化オブジェクトを JsonTools.Encode() に渡せるような構造化オブジェクトを持つ ObjectTree に変換する。 // orig: * \u305f\u3060\u306e\u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3092 JsonTools.Encode() \u306b\u6e21\u305b\u308b\u3088\u3046\u306a\u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3092\u6301\u3064 ObjectTree \u306b\u5909\u63db\u3059\u308b\u3002
-	 * @param root 構造化オブジェクト // orig: * @param root \u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8
-	 * @return 構造化オブジェクト // orig: * @return \u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8
+	 * 色々なオブジェクトからインスタンスを生成する。 // orig: * \u8272\u3005\u306a\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u304b\u3089\u30a4\u30f3\u30b9\u30bf\u30f3\u30b9\u3092\u751f\u6210\u3059\u308b\u3002
+	 * @param root 色々なオブジェクト // orig: * @param root \u8272\u3005\u306a\u30aa\u30d6\u30b8\u30a7\u30af\u30c8
+	 * @return インスタンス // orig: * @return \u30a4\u30f3\u30b9\u30bf\u30f3\u30b9
 	 */
 	public static ObjectTree convert(Object root) {
 		return new ObjectTree(conv(root));
@@ -67,8 +78,8 @@ public class ObjectTree implements Iterable<ObjectTree> {
 	private Object _root;
 
 	/**
-	 * 構造化オブジェクトを生成する。 // orig: * \u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3092\u751f\u6210\u3059\u308b\u3002
-	 * @param root JsonTools.Encode() に渡せるような構造化オブジェクトであること。そうでない場合は Convert() を使用すること。 // orig: * @param root JsonTools.Encode() \u306b\u6e21\u305b\u308b\u3088\u3046\u306a\u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u3067\u3042\u308b\u3053\u3068\u3002\u305d\u3046\u3067\u306a\u3044\u5834\u5408\u306f Convert() \u3092\u4f7f\u7528\u3059\u308b\u3053\u3068\u3002
+	 * 構造化オブジェクトからインスタンスを生成する。 // orig: * \u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8\u304b\u3089\u30a4\u30f3\u30b9\u30bf\u30f3\u30b9\u3092\u751f\u6210\u3059\u308b\u3002
+	 * @param root 構造化オブジェクト // orig: * @param root \u69cb\u9020\u5316\u30aa\u30d6\u30b8\u30a7\u30af\u30c8
 	 */
 	public ObjectTree(Object root) {
 		_root = root;
